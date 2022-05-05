9e9c08d2439dd3df41bfd94c9e70cf0311dc8465
xccommit <schosnablue@gmail.com>
2022-05-05 23:27:37 +0800

    implement index.js and _app.js

--- FullStackWeb3Development/web3-blog/pages/_app.js
+++ FullStackWeb3Development/web3-blog/pages/_app.js
@@ -1,7 +1,165 @@
-import '../styles/globals.css'
+import {useState} from 'react'
+import Link from 'next/link'
+import {css} from '@emotion/css'
+import {ethers} from 'ethers'
+import Web3Modal from 'web3modal'
+import WalletConnectProvider from '@walletconnect/web3-provider'
+import {AccountContext} from '../Context.js'
+import {ownerAddress} from '../config'
+import 'easymde/dist/easymde.min.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  const [account, setAccount] = useState(null)
+
+  async function getWeb3Modal() {
+    const web3Modal = new Web3Modal({
+      cacheProvider: false,
+      providerOptions: {
+        walletConnect: {
+          package: WalletConnectProvider,
+          options: {
+            infuraId: '6b0681cef1da4236b6c2157508f4dad0'
+          },
+        },
+      },
+    })
+    return web3Modal
+  }
+  
+  async function connect() {
+    try {
+      const web3Modal = await getWeb3Modal()
+      const connection  = await web3Modal.connect()
+      const provider = new ethers.providers.Web3Provider(connection)
+      const accounts = await provider.listAccounts()
+      setAccount(accounts[0])
+    } catch (err) {
+      consol.log('error:', err)
+    }
+  }
+  return (
+    <div>
+      <nav className={nav}>
+        <div className={header}>
+          <Link href="/">
+            <a>
+              <img
+                src='/logo.svg'
+                alt="React Logo"
+                style={{ width: '50px'}}
+              />
+            </a>
+          </Link>
+          <Link href="/">
+            <a>
+              <div className={titleContainer}>
+                <h2 className={title}>Full Stack</h2>
+                <p className={description}>WEB3</p>
+              </div>
+            </a>
+          </Link>
+          {
+            !account && (
+              <div className={buttonContainer}>
+                <button className={buttonStyle} onClick={connect}>Connect</button>
+              </div>
+            )
+          }
+          {
+            account && <p className={accountInfo}>{account}</p>
+          }
+        </div>
+        <div className={linkContainer}>
+          <Link href="/">
+            <a className={link}>
+              Home
+            </a>
+          </Link>
+          {
+            (account === ownerAddress) && (
+              <Link href="/create-post">
+                <a className={link}>
+                  Create Post
+                </a>
+              </Link>
+            )
+          }
+        </div>
+      </nav>
+      <div className={container}>
+          <AccountContext.Provider value={account}>
+            <Component {...pageProps} connect={connect} />
+          </AccountContext.Provider>
+      </div>
+    </div>
+  )
 }
 
-export default MyApp
+const accountInfo = css`
+  width: 100%;
+  display: flex;
+  justify-content: flex-end;
+  font-size: 12px;
+`
+
+const container = css`
+  padding: 40px;
+`
+
+const linkContainer = css`
+  padding: 30px 60px;
+  background-color: #fafafa;
+`
+
+const nav = css`
+  background-color: white;
+`
+
+const header = css`
+  display: flex;
+  border-bottom: 1px solid rgba(0, 0, 0, .075);
+  padding: 20px 30px;
+`
+
+const description = css`
+  margin: 0;
+  color: #999999;
+`
+
+const titleContainer = css`
+  display: flex;
+  flex-direction: column;
+  padding-left: 15px;
+`
+
+const title = css`
+  margin-left: 30px;
+  font-weight: 500;
+  margin: 0;
+`
+
+const buttonContainer = css`
+  width: 100%;
+  display: flex;
+  flex: 1;
+  justify-content: flex-end;
+`
+
+const buttonStyle = css`
+  background-color: #fafafa;
+  outline: none;
+  border: none;
+  font-size: 18px;
+  padding: 16px 70px;
+  border-radius: 15px;
+  cursor: pointer;
+  box-shadow: 7px 7px rgba(0, 0, 0, .1);
+`
+
+const link = css`
+  margin: 0px 40px 0px 0px;
+  font-size: 16px;
+  font-weight: 400;
+`
+
+export default App
---
+++ FullStackWeb3Development/web3-blog/scripts/deploy.js
@@ -0,0 +1,25 @@
+const hre = require("hardhat");
+const fs = require("fs");
+
+async function main() {
+  const Blog = await hre.ethers.getContractFactory("Blog");
+  const blog = await Blog.deploy("My web3 blog!");
+
+  await blog.deployed();
+
+  console.log("Blog deployed to:", blog.address);
+
+  fs.writeFileSync('./config.js',`
+    export const contractAddress = "${blog.address}";
+    export const ownerAddress = "${blog.signer.address}"
+  `)
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
