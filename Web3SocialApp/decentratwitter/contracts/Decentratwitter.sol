b80157efea69c17538869b8953d5d1745eaad009
xccommit <schosnablue@gmail.com>
2022-05-09 23:40:05 +0800

    implement test cases

--- Web3SocialApp/decentratwitter/contracts/Decentratwitter.sol
+++ Web3SocialApp/decentratwitter/contracts/Decentratwitter.sol
@@ -6,5 +6,39 @@ import "hardhat/console.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract Decentratwitter is ERC721URIStorage {
+    uint256 public tokenCount;
+    uint256 public postCount;
+
+    mapping(uint256 => Post) public posts;
+    mapping(uint256 => uint256) public profiles;
+
+    struct Post{
+        uint256 id;
+        string hash;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    event PostCreated(
+        uint256 id,
+        string hash,
+        uint256 tipAmount,
+        address payable author
+    );
+
+    event PostTipped(
+        uint256 id,
+        string hash,
+        uint256 tipAmount,
+        address payable aythor
+    );
+
     constructor() ERC721("Decentratwitter", "DAPP") {}
+
+    function mint(string memory _tokenURI) external returns (uint256) {
+        tokenCount++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return (tokenCount);
+    }
 }
--- Web3SocialApp/decentratwitter/test/Decentratwitter.test.js
+++ Web3SocialApp/decentratwitter/test/Decentratwitter.test.js
@@ -2,4 +2,33 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Decentratwitter", function () {
+    let decentratwitter;
+    let deployer, user1, user2, users;
+    let URI = "SampleURI";
+    let postHash = "SampleHash";
+    beforeEach(async () => {
+        [deployer, user1, user2, ...users] = await ethers.getSigners();
+        const DecentratwitterFactory = await ethers.getContractFactory("Decentratwitter");
+        decentratwitter = await DecentratwitterFactory.deploy();
+        await decentratwitter.connect(user1).mint(URI);
+    })
+    describe('Deployment', async() => {
+        it("Should track name and symbol", async function () {
+            const nftName = "Decentratwitter"
+            const nftSymbol = "DAPP"
+            expect(await decentratwitter.name()).to.equal(nftName);
+            expect(await decentratwitter.symbol()).to.equal(nftSymbol);
+        });
+    })
+    describe('Minting NFTs', async () => {
+        it("Should track exah minted NFT", async function () {
+            expect(await decentratwitter.tokenCount()).to.equal(1);
+            expect(await decentratwitter.balanceOf(user1.address)).to.equal(1);
+            expect(await decentratwitter.tokenURI(1)).to.equal(URI);
+            await decentratwitter.connect(user2).mint(URI);
+            expect(await decentratwitter.tokenCount()).to.equal(2);
+            expect(await decentratwitter.balanceOf(user2.address)).to.equal(1);
+            expect(await decentratwitter.tokenURI(2)).to.equal(URI);
+        });
+    })
 });
